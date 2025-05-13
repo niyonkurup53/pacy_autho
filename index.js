a5b6fd0de1791dbff65b0f38cb64823cc7ee7861
niyonkurup53 <niyonkurup53@gmail.com>
2025-05-13 11:51:08 +0200

    db.js,index.js,product

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.use('/products', authenticateToken, productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:${PORT}');
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
